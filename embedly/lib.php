5bc280e9ed721ef7ef8592e1ed81116b62e1dcf2
Gregor Anzelj <gregor.anzelj@gmail.com>
2014-11-02 20:03:27 +0100

    Embedly plugin for Mahara version 1.0.10 (2014110200)
    - Minor update so the code works in Mahara 1.10
    - Changed thumb image

--- embedly/lib.php
+++ embedly/lib.php
@@ -1,27 +1,11 @@
 <?php
 /**
- * Mahara: Electronic portfolio, weblog, resume builder and social networking
- * Copyright (C) 2006-2009 Catalyst IT Ltd and others; see:
- *                         http://wiki.mahara.org/Contributors
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
  * @subpackage blocktype-embedly
  * @author     Gregor Anzelj
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @copyright  (C) 2011 Gregor Anzelj, gregor.anzelj@gmail.com
+ * @copyright  (C) 2011-2014 Gregor Anzelj, gregor.anzelj@gmail.com
  *
  */
 
@@ -213,7 +197,7 @@ class PluginBlocktypeEmbedly extends SystemBlocktype {
             'elements' => array(
                 'embedlysiteapikeydesc' => array(
 					'type'  => 'html',
-                    'value' => get_string('apikeydescription','blocktype.embedly', '<a href="http://embed.ly/pricing/free" target="_blank">', '</a>')
+                    'value' => get_string('apikeydescription','blocktype.embedly', '<a href="https://app.embed.ly/login" target="_blank">', '</a>')
                 ),
                 'embedlysiteapikey' => array(
                     'title'        => get_string('apikey', 'blocktype.embedly'),
@@ -248,7 +232,7 @@ class PluginBlocktypeEmbedly extends SystemBlocktype {
 
     }
 
-    public static function save_config_options($values) {
+    public static function save_config_options($form, $values) {
 		// Set Embedly API key - this is site API key!
         set_config_plugin('blocktype', 'embedly', 'embedlysiteapikey', $values['embedlysiteapikey']);
 		// If user ID is set, than clear Embedly API key for that user...
